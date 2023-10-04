2c1246544f439ef5b5050794b792e812f20edb65
DiggyShants <dalton.flanagan@gmail.com>
2023-10-04 20:53:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 import pandas as pd
 
 app = Flask(__name__)
 
-# In-memory storage for the prototype. In a real application, use a database.
-inventory = []
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    file = request.files['file']
-    global inventory
-    df = pd.read_excel(file)
-    inventory = df.to_dict(orient='records')
-    return jsonify({"message": "Uploaded successfully!"}), 200
+CSV_PATH = "Inventory.csv"
 
 @app.route('/inventory', methods=['GET'])
 def get_inventory():
+    df = pd.read_csv(CSV_PATH)
+    inventory = df.to_dict(orient='records')
     return jsonify(inventory)
+
+@app.route('/request-items', methods=['POST'])
+def request_items():
+    data = request.json
+    item_name = data.get('item_name')
+    amount = data.get('amount')
+
+    df = pd.read_csv(CSV_PATH)
+    idx = df[df['Item name'] == item_name].index[0]
+    if df.at[idx, 'quantity of item'] < amount:
+        return jsonify({"message": "Not enough items in inventory"}), 400
+
+    df.at[idx, 'quantity of item'] -= amount
+    df.to_csv(CSV_PATH, index=False)
+    return jsonify({"message": "Request successful"}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
